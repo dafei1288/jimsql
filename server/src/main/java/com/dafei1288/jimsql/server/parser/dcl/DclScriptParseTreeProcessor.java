9238fa1054f2e3babd9211d19e212e78c3d8ba7f
dafei1288 <dafei1288@sina.com>
2025-12-02 17:42:32 +0800

    feat(parser/dcl): implement DclScriptParseTreeProcessor producing DclRequest for SHOW DATABASES/TABLES/DESCRIBE/SHOW CREATE

--- server/src/main/java/com/dafei1288/jimsql/server/parser/dcl/DclScriptParseTreeProcessor.java
+++ server/src/main/java/com/dafei1288/jimsql/server/parser/dcl/DclScriptParseTreeProcessor.java
@@ -1,28 +1,57 @@
 package com.dafei1288.jimsql.server.parser.dcl;
 
-import org.snt.inmemantlr.exceptions.ParseTreeProcessorException;
+import com.dafei1288.jimsql.server.parser.ScriptParseTreeProcessor;
+import com.dafei1288.jimsql.server.parser.SqlStatementEnum;
 import org.snt.inmemantlr.tree.ParseTree;
 import org.snt.inmemantlr.tree.ParseTreeNode;
-import org.snt.inmemantlr.tree.ParseTreeProcessor;
 
-public class DclScriptParseTreeProcessor extends ParseTreeProcessor {
+public class DclScriptParseTreeProcessor extends ScriptParseTreeProcessor {
 
-  public DclScriptParseTreeProcessor(ParseTree parseTree) {
-    super(parseTree);
+  public static class DclRequest {
+    public SqlStatementEnum type;
+    public String tableName;
   }
 
+  private DclRequest req;
+
+  public DclScriptParseTreeProcessor(ParseTree parseTree) { super(parseTree); }
+
   @Override
-  public Object getResult() {
+  public Object getResult() { return req; }
+
+  @Override
+  protected void process(ParseTreeNode n) {
+    if (smap.get(n) == null) {
+      ParseTree sub = this.parseTree.getSubtrees(it -> it == n).stream().findFirst().orElse(null);
+      if (sub == null) sub = this.parseTree.getSubtrees(it -> it.getRule().equals(n.getRule())).stream().findFirst().orElse(null);
+      if (sub != null) smap.put(n, sub);
+    }
+    String r = n.getRule();
+    if ("showDatabases".equals(r)) {
+      req = new DclRequest(); req.type = SqlStatementEnum.SHOW_DATABASES; this.setSqlStatementEnum(SqlStatementEnum.SHOW_DATABASES); this.setCurrentParseTreeProcessor(this);
+    } else if ("showTables".equals(r)) {
+      req = new DclRequest(); req.type = SqlStatementEnum.SHOW_TABLES; this.setSqlStatementEnum(SqlStatementEnum.SHOW_TABLES); this.setCurrentParseTreeProcessor(this);
+    } else if ("showCreateTable".equals(r)) {
+      req = new DclRequest(); req.type = SqlStatementEnum.SHOW_CREATE_TABLE; this.setSqlStatementEnum(SqlStatementEnum.SHOW_CREATE_TABLE); this.setCurrentParseTreeProcessor(this);
+      String t = findTableName(n); if (t != null) req.tableName = stripQuotes(t);
+    } else if ("describeTable".equals(r) || "showTableDesc".equals(r)) {
+      req = new DclRequest(); req.type = SqlStatementEnum.SHOW_TABLEDESC; this.setSqlStatementEnum(SqlStatementEnum.SHOW_TABLEDESC); this.setCurrentParseTreeProcessor(this);
+      String t = findTableName(n); if (t != null) req.tableName = stripQuotes(t);
+    }
+  }
+
+  private String findTableName(ParseTreeNode n) {
+    if ("tableName".equals(n.getRule())) return n.getLabel();
+    for (ParseTreeNode c : n.getChildren()) {
+      String v = findTableName(c); if (v != null) return v;
+    }
     return null;
   }
 
-  @Override
-  protected void initialize() {
-
-  }
-
-  @Override
-  protected void process(ParseTreeNode parseTreeNode) throws ParseTreeProcessorException {
-    System.out.println("--> "+parseTreeNode.getRule());
+  private String stripQuotes(String s) {
+    if (s == null || s.length() < 2) return s;
+    char f = s.charAt(0), l = s.charAt(s.length()-1);
+    if ((f == '`' && l == '`') || (f == '"' && l == '"')) return s.substring(1, s.length()-1);
+    return s;
   }
 }
